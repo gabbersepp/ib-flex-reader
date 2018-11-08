2cf6029e1dbef35bbb5b33f32db8b2ef2f68fa58
Josef Biehler <gabbersepp@googlemail.com>
2018-11-08 14:20:15 +0100

    allow enum with 64 flags

--- IbFlexReader/IbFlexReader.Contracts/Enums.cs
+++ IbFlexReader/IbFlexReader.Contracts/Enums.cs
@@ -60,19 +60,106 @@ namespace IbFlexReader.Contracts
         [EnumName("Price Adjustments")]
         PriceAdjustments
     }
-
-    [Flags]
+    
     [EnumName]
-    public enum Notes
+    public enum Notes : long
     {
-        [EnumName("Ex")]
-        Exercised = 1,
         [EnumName("A")]
-        Assigned = 2,
-        [EnumName("P")]
-        PartialExecution = 4,
+        Assigned = 1,
+        [EnumName("AEx")]
+        AutomaticalExercise = 2^1,
+        [EnumName("Adj")]
+        Adjustment = 2^2,
+        [EnumName("Aw")]
+        AwayTrade = 2^3,
+        [EnumName("B")]
+        AutoBuyIn = 2^4,
+        [EnumName("Bo")]
+        DirectLending = 2^5,
+        [EnumName("CD")]
+        CashDelivery = 2^6,
+        [EnumName("CP")]
+        ComplexePosition = 2^7,
+        [EnumName("Ca")]
+        Deleted = 2^8,
+        [EnumName("Co")]
+        CorrectedTrade = 2^9,
+        [EnumName("Cx")]
+        CrossTrade = 2^10,
+        [EnumName("ETF")]
+        EtfCreation = 2^11,
         [EnumName("Ep")]
-        Expired = 8
+        Expired = 2^12,
+        [EnumName("Ex")]
+        Exercised = 2^13,
+        [EnumName("G")]
+        GuaranteedAccountSegment = 2^14,
+        [EnumName("HC")]
+        MaxTaxBase = 2^15,
+        [EnumName("HFI")]
+        InvestmentTransferToHedgeFund = 2^16,
+        [EnumName("HFR")]
+        RedemptionForHedgeFund = 2^17,
+        [EnumName("I")]
+        InternalTransfer = 2^18,
+        [EnumName("IA")]
+        ExecutedAgainstCompany = 2^19,
+        [EnumName("INV")]
+        InvestmentTransferFromInvestor = 2^20,
+        [EnumName("IPO")]
+        IPO = 2^21,
+        [EnumName("L")]
+        MarginViolation = 2^22,
+        [EnumName("LD")]
+        AdjustedLossWashSale = 2^23,
+        [EnumName("LI")]
+        LiFo = 2^24,
+        [EnumName("LT")]
+        LongTermPL = 2^25,
+        [EnumName("M")]
+        ManualThroughIB = 2^26,
+        [EnumName("MEx")]
+        ManualExercise = 2^27,
+        [EnumName("ML")]
+        MaximizeLoss = 2^28,
+        [EnumName("MLG")]
+        MLG = 2^29,
+        [EnumName("MLL")]
+        MLL = 2^30,
+        [EnumName("MSG")]
+        MSG = 2^31,
+        [EnumName("MSL")]
+        MSL = 2^32,
+        [EnumName("O")]
+        OpeningTrade = 2^33,
+        [EnumName("P")]
+        PartialExecution = 2^34,
+        [EnumName("PI")]
+        PI = 2^35,
+        [EnumName("Po")]
+        InterestDividendAccrualPosting = 2^36,
+        [EnumName("Pr")]
+        PrincipalTrade = 2^37,
+        [EnumName("R")]
+        DividendReinvestment = 2^38,
+        [EnumName("RED")]
+        RedemptionToInvestor = 2^39,
+        [EnumName("Re")]
+        TransformationInterestDividend = 2^40,
+        [EnumName("Ri")]
+        Refund = 2^41,
+        [EnumName("SI")]
+        ArrangedByIB = 2^42,
+        [EnumName("SL")]
+        SL = 2^43,
+        [EnumName("SO")]
+        ArrangedByIntroducingBroker = 2^44,
+        [EnumName("SS")]
+        ShortendedExecution = 2^45,
+        [EnumName("ST")]
+        ShortTermPL = 2^46,
+        [EnumName("T")]
+        Transfer = 2^47
     }
 
     public class EnumParser
@@ -126,12 +213,14 @@ namespace IbFlexReader.Contracts
             {
                 throw new Exception($"both enum types are not equal: {enum1}, {enum2}");
             }
-            
-            // cast object to int
+
+            var enumsType = enum1.GetType().GetEnumUnderlyingType();
+
+            // cast object to int/long
             var p1 = Expression.Parameter(typeof(object), "e1");
-            var convExpr1 = Expression.Convert(p1, typeof(int));
+            var convExpr1 = Expression.Convert(p1, enumsType);
             var p2 = Expression.Parameter(typeof(object), "e2");
-            var convExpr2 = Expression.Convert(p2, typeof(int));
+            var convExpr2 = Expression.Convert(p2, enumsType);
             var or = Expression.Or(convExpr1, convExpr2);
             // cast int to enum type
             var toEnumConv = Expression.Convert(or, t1);
--- IbFlexReader/IbFlexReader.Test/EnumTest.cs
+++ IbFlexReader/IbFlexReader.Test/EnumTest.cs
@@ -8,7 +8,7 @@ namespace IbFlexReader.Test
     {
         [Test]
         public void TestParseWithMapping()
-        {
+        { 
             var enumValue = EnumParser.ParseWithMapping(typeof(CashTransactionType), "Other Fees");
 
             enumValue.Should().Be(CashTransactionType.OtherFees);
@@ -20,6 +20,14 @@ namespace IbFlexReader.Test
             var enumValue = EnumParser.Parse(typeof(OpenClose), "O;C");
 
             enumValue.Should().HaveFlag(OpenClose.C).And.HaveFlag(OpenClose.O);
+        }
+
+        [Test]
+        public void TestNotesMoreThan32Flags()
+        {
+            var enumValue = EnumParser.Parse(typeof(Notes), "Ex;A");
+
+            enumValue.Should().HaveFlag(Notes.Assigned).And.HaveFlag(Notes.Exercised);
         }
     }
 }
