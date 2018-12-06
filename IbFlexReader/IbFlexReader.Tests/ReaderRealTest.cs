9ed734eee09095f8622745b77940f2a75d737f21
winJumu <winJumu@abc.de>
2018-12-06 16:00:40 +0100

    added usings, added test

--- IbFlexReader/IbFlexReader.Tests/ReaderRealTest.cs
+++ IbFlexReader/IbFlexReader.Tests/ReaderRealTest.cs
@@ -2,17 +2,28 @@
 
 namespace IbFlexReader.Tests
 {
-	public class ReaderRealTest
-	{
-		private string _queryId = string.Empty;
-		private string _token = string.Empty;
+    public class ReaderRealTest
+    {
+        // fill with proper values to run the tests
+        private string _queryId = string.Empty;
+        private string _token = string.Empty;
 
-		[Test]
-		public void Test()
-		{
-			var result = new Reader().GetByApi(_token, _queryId);
-			result.Wait();
-			Assert.IsNotNull(result);
-		}
-	}
+        [Test]
+        public void GetByApi()
+        {
+            Assert.Inconclusive("add valid queryID and token to run this test");
+            var result = new Reader().GetByApi(_token, _queryId, @"c:\Tests\Dump.xml");
+            result.Wait();
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void GetByString()
+        {
+            Assert.Inconclusive("add valid queryID and token to run this test");
+            string xml = System.IO.File.ReadAllText(@"c:\Tests\Dump.2.xml");
+            var result = new Reader().GetByString(xml);
+            Assert.IsNotNull(result);
+        }
+    }
 }
