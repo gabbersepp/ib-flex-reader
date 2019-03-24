2b2f0ba13a51e2a9267965ac114c9c0040302d7b
Jürgen Müller <mueller@mueller-se.de>
2019-03-24 15:16:20 +0100

    retry added to Reader.GetByApi

--- IbFlexReader/IbFlexReader/Reader.cs
+++ IbFlexReader/IbFlexReader/Reader.cs
@@ -2,40 +2,40 @@
 using IbFlexReader.Xml;
 using IbFlexReader.Xml.Contracts;
 using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace IbFlexReader
 {
-    public class Reader
-    {
-        private readonly IStreamBuilder<string> sb;
+	public class Reader
+	{
+		private readonly IStreamBuilder<string> sb;
 
 
-        public Reader()
-        {
-            sb = new StringStream();
-        }
+		public Reader()
+		{
+			sb = new StringStream();
+		}
 
-        /// <summary>
-        /// Converts a string to a FlexQueryResponse.
-        /// </summary>
-        /// <param name="xmlFile"></param>
-        /// <param name="options"></param>
-        /// <returns>An object with account activities</returns>
-        public Contracts.FlexQueryResponse GetByString(string xmlFile, Options options = null)
-        {
-            using (var stream = this.sb.GenerateStream(xmlFile))
-            {
-                var result = Deserializer.Deserialize<Xml.Contracts.FlexQueryResponse, Contracts.FlexQueryResponse>(stream, out var errorObjects);
-                result = result ?? new Contracts.FlexQueryResponse();
-                result.Errors = errorObjects;
-                Logic.ProcessStatement(result.FlexStatements?.FlexStatement, options);
+		/// <summary>
+		/// Converts a string to a FlexQueryResponse.
+		/// </summary>
+		/// <param name="xmlFile"></param>
+		/// <param name="options"></param>
+		/// <returns>An object with account activities</returns>
+		public Contracts.FlexQueryResponse GetByString(string xmlFile, Options options = null)
+		{
+			using (var stream = this.sb.GenerateStream(xmlFile))
+			{
+				var result = Deserializer.Deserialize<Xml.Contracts.FlexQueryResponse, Contracts.FlexQueryResponse>(stream, out var errorObjects);
+				result = result ?? new Contracts.FlexQueryResponse();
+				result.Errors = errorObjects;
+				Logic.ProcessStatement(result.FlexStatements?.FlexStatement, options);
 
-                return result;
-            }
-        }
-
+				return result;
+			}
+		}
 
         /// <summary>
         /// Reads a FlexQuery by API.
@@ -43,51 +43,174 @@ namespace IbFlexReader
         /// <param name="token">token provided by IB</param>
         /// <param name="queryId">ID of the FlexQuery (not the name)</param>
         /// <param name="dumpFile">if set, the data returned from IB will be dumped to file</param>
+        /// <param name="retry">if GetStatement returns warning 1019 (... Please try again shortly), retry defines how many times it will be tried again</param>
         /// <returns></returns>
-        public async Task<FlexResult> GetByApi(string token, string queryId, string dumpFile = null)
-        {
-            using (var client = new HttpClient())
-            {
-                var uri = new Uri($"https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.SendRequest?t={token}&q={queryId}&v=3");
+        public async Task<FlexResult> GetByApi(string token, string queryId, string dumpFile = null, int? retry = null)
+		{
+			using (var client = new HttpClient())
+			{
+				FlexResult requestResult = await SendRequest(client, token, queryId, dumpFile);
 
-                string referenceCode;
-                using (var sendRequestResult = await client.PostAsync(uri, null))
-                using (var stream = await sendRequestResult.Content.ReadAsStreamAsync())
+				if (!requestResult.IsSuccess)
+				{
+					return requestResult;
+				}
+
+				string referenceCode = requestResult.ErrorMessage;
+
+				int retryCounter = 0;
+				if (retry.HasValue && retry.Value > 0) retryCounter = retry.Value;
+
+				FlexResult result = null;
+
+				bool tryAgain = true;
+				while (tryAgain)
+				{
+					result = await GetStatement(client, token, referenceCode, dumpFile);
+
+					if (!result.IsSuccess && result.ErrorCode.HasValue && result.ErrorCode.Value == 1019)
+					{
+                        // warning is: 'Statement generation in progress. Please try again shortly. '
+
+                        System.Diagnostics.Debug.Print("IbFlexReader.Reader.GetByApi: Retry");
+
+						// retry getting data?
+						if (retryCounter > 0)
+						{
+							// yes
+							retryCounter--;
+                            // wait a little bit 
+							System.Threading.Thread.Sleep(2000);
+						}
+						else
+						{
+							// no
+							tryAgain = false;
+						}
+					}
+					else
+					{
+						// data received
+						tryAgain = false;
+					}
+				}
+
+				return result;
+
+			}
+		}
+
+		private async Task<FlexResult> GetStatement(HttpClient client, string token, string referenceCode, string dumpFile)
+		{
+			var uri = new Uri($"https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.GetStatement?q={referenceCode}&t={token}&v=3");
+
+			using (var getStatementResult = await client.PostAsync(uri, null))
+			using (var stream = await getStatementResult.Content.ReadAsStreamAsync())
+			{
+				if (dumpFile != null) { Utils.DumpFileWriter.DumpGetStatement(stream, dumpFile); }
+
+				// in case of an error or warning, a FlexStatementResponse object will be returned instead of the data
+				if (TryGetFlexStatementResponse(stream, out FlexStatementResponse flexStatementResponse))
+				{
+					return new FlexResult
+					{
+						Status = flexStatementResponse.Status,
+						ErrorCode = flexStatementResponse.ErrorCode,
+						ErrorMessage = flexStatementResponse.ErrorMessage
+					};
+				}
+
+                var queryResponse = Deserializer.Deserialize<Xml.Contracts.FlexQueryResponse, Contracts.FlexQueryResponse>(stream, out var errorObjects);
+
+                // in case of an error during deserialization, queryResponse maybe NULL (see PopulateFrom method)
+                if (queryResponse == null)
                 {
-                    if (dumpFile != null) { Utils.DumpFileWriter.DumpSendRequest(stream, dumpFile); }
+					if (errorObjects.Count > 0)
+					{
+						System.Text.StringBuilder sb = new System.Text.StringBuilder();
+						errorObjects.ForEach(e => sb.AppendLine(e.Message));
+						throw new Exception(sb.ToString());
+					}
+					else
+					{
+						throw new Exception("deserialization failed");
+					}
+				}
 
-                    var response = Deserializer.Deserialize<XmlFlexStatementResponse, FlexStatementResponse>(stream, out var errorObjects);
+				queryResponse.Errors = errorObjects;
 
-                    if (response.Status != "Success")
-                    {
-                        return new FlexResult
-                        {
-                            ErrorCode = response.ErrorCode,
-                            ErrorMessage = response.ErrorMessage
-                        };
-                    }
+				return new FlexResult
+				{
+					Status = "Success",
+					FlexQueryResponse = queryResponse
+				};
+			}
+		}
 
-                    referenceCode = response.ReferenceCode.Value.ToString();
-                }
+		private async Task<FlexResult> SendRequest(HttpClient client, string token, string queryId, string dumpFile)
+		{
+			var uri = new Uri($"https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.SendRequest?t={token}&q={queryId}&v=3");
 
+			using (var sendRequestResult = await client.PostAsync(uri, null))
+			using (var stream = await sendRequestResult.Content.ReadAsStreamAsync())
+			{
+				if (dumpFile != null) { Utils.DumpFileWriter.DumpSendRequest(stream, dumpFile); }
 
-                uri = new Uri($"https://gdcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.GetStatement?q={referenceCode}&t={token}&v=3");
+				var response = Deserializer.Deserialize<XmlFlexStatementResponse, FlexStatementResponse>(stream, out var errorObjects);
 
-                using (var getStatementResult = await client.PostAsync(uri, null))
-                using (var stream = await getStatementResult.Content.ReadAsStreamAsync())
-                {
-                    if (dumpFile != null) { Utils.DumpFileWriter.DumpGetStatement(stream, dumpFile); }
+				if (response == null)
+				{
+					if (errorObjects.Count > 0)
+					{
+						System.Text.StringBuilder sb = new System.Text.StringBuilder();
+						errorObjects.ForEach(e => sb.AppendLine(e.Message));
+						throw new Exception(sb.ToString());
+					}
+					else
+					{
+						throw new Exception("deserialization failed");
+					}
+				}
 
-                    var queryResponse = Deserializer.Deserialize<Xml.Contracts.FlexQueryResponse, Contracts.FlexQueryResponse>(stream, out var errorObjects);
-                    queryResponse.Errors = errorObjects;
+				if (response.Status == "Success")
+				{
+					//referenceCode = response.ReferenceCode.Value.ToString();
 
-                    return new FlexResult
-                    {
-                        Status = "Success",
-                        FlexQueryResponse = queryResponse
-                    };
-                }
-            }
-        }
-    }
+					return new FlexResult
+					{
+						Status = "Success",
+						ErrorMessage = response.ReferenceCode.Value.ToString()
+					};
+				}
+				else
+				{
+					return new FlexResult
+					{
+						Status = response.Status,
+						ErrorCode = response.ErrorCode,
+						ErrorMessage = response.ErrorMessage
+					};
+				}
+			}
+		}
+
+		private bool TryGetFlexStatementResponse(Stream stream, out FlexStatementResponse response)
+		{
+			response = null;
+
+			try
+			{
+				response = Deserializer.Deserialize<XmlFlexStatementResponse, FlexStatementResponse>(stream, out var errorObjects);
+				return response == null ? false : true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+			finally
+			{
+				if (stream != null) stream.Position = 0;
+			}
+		}
+	}
 }
