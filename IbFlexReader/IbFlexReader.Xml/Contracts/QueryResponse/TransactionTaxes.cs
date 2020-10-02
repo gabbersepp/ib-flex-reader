ea2d80f3088b6f0c0bf823a473624b7a1acdef3e
Dan Martin <nimbusdodger@outlook.com>
2020-10-02 17:16:06 -0400

    Implemented support for TransactionTaxes/TransactionTax.

---
+++ IbFlexReader/IbFlexReader.Xml/Contracts/QueryResponse/TransactionTaxes.cs
@@ -0,0 +1,12 @@
+﻿namespace IbFlexReader.Xml.Contracts.QueryResponse
+{
+    using System.Collections.Generic;
+    using System.Xml.Serialization;
+
+    [XmlRoot(ElementName = "TransactionTaxes")]
+    public class TransactionTaxes
+    {
+        [XmlElement(ElementName = "TransactionTax")]
+        public List<TransactionTax> TransactionTax { get; set; }
+    }
+}
